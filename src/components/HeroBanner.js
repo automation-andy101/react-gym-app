3607143ca49621a089502c84962c619cd7d54b30
Andy Short <automation.andy101@gmail.com>
2023-04-04 09:10:48 +0100

    Added HeroBanner component functionality

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box, Stack, Typography, Button } from '@mui/material';
 
+import HeroBannerImage from '../assets/images/banner.png';
+import { Padding } from '@mui/icons-material';
+
 
 const HeroBanner = () => {
     return (
@@ -24,21 +27,45 @@ const HeroBanner = () => {
                 sx={{
                     fontSize: { lg: '40px', xs: '40px' }
                 }}
+                mb='23px'
+                mt='30px'
             >
                 Sweat, Smile <br /> and Repeat
             </Typography>
             <Typography
                 fontSize='22px'
                 lineHeight='35px'
+                mb={4}
             >
                 Check out the most effective exercises
             </Typography>
             <Button
                 variant='contained'
                 color='error'
+                href='#exercises'
+                sx={{
+                    backgroundColor: '#ff2625',
+                    Padding: '10px'
+                }}
             >
                 Explore Exercises
             </Button>
+            <Typography
+                fontWeight='600'
+                color='#ff2625'
+                sx={{
+                    opacity: 0.1,
+                    display: { lg: 'block', xs: 'none' }
+                }}
+                fontSize='200px'
+            >
+                Exercise
+            </Typography>
+            <img 
+                src={HeroBannerImage} 
+                alt='banner'
+                className='hero-banner-img'    
+            />
         </Box>
     )
 }
