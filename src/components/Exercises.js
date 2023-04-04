3244abff35a660829c57bdba609b196d71e2605f
Andy Short <automation.andy101@gmail.com>
2023-04-04 08:45:25 +0100

    first commit

---
+++ src/components/Exercises.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Exercises = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Exercises;
---
+++ src/components/SearchExercises.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SearchExercises = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchExercises;
